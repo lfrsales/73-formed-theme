9ea989ec39ee1ae1ca201cbe828a2994747ae7a8
Evan Thibodeau <evan.thibodeau@liferay.com>
2020-09-29 16:08:59 +0400

    Init based on Classic

---
+++ src/js/main.js
@@ -0,0 +1,33 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+/*
+ * This function gets loaded when all the HTML, not including the portlets, is
+ * loaded.
+ */
+AUI().ready(function () {});
+
+/*
+ * This function gets loaded after each and every portlet on the page.
+ *
+ * portletId: the current portlet's id
+ * node: the Alloy Node object of the current portlet
+ */
+Liferay.Portlet.ready(function (_portletId, _node) {});
+
+/*
+ * This function gets loaded when everything, including the portlets, is on
+ * the page.
+ */
+Liferay.on('allPortletsReady', function () {});

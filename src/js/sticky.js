45a32a075401280f21732f582791e37675eacf77
Evan Thibodeau <evan.thibodeau@liferay.com>
2021-10-12 12:15:46 +0400

    Update for 7.4 Compatibility.

--- src/js/sticky.js
+++ src/js/sticky.js
@@ -4,24 +4,26 @@ AUI().ready(function () {
 	const header = document.getElementById("lowerHeader");
 	const wrapper = document.getElementById("wrapper");
 
-	const headerOffset = header.offsetTop;
-	const headerHeight = header.offsetHeight;
-
-	window.onscroll = function() {
-		if (window.pageYOffset > headerOffset) {
-			const CPCHeight = controlPanelContainer ? controlPanelContainer.offsetHeight : 0;
-
-			header.classList.add("sticky");
-
-			header.style.setProperty('top', `${CPCHeight}px`);
-
-			wrapper.style.setProperty('padding-top' ,`${headerHeight}px`);
-		} else {
-			header.classList.remove("sticky");
-			
-			header.style.removeProperty('top');
-
-			wrapper.style.removeProperty('padding-top');
-		}
-	};
+	if (header) {
+		const headerOffset = header.offsetTop;
+		const headerHeight = header.offsetHeight;
+	
+		window.onscroll = function() {
+			if (window.pageYOffset > headerOffset) {
+				const CPCHeight = controlPanelContainer ? controlPanelContainer.offsetHeight : 0;
+	
+				header.classList.add("sticky");
+	
+				header.style.setProperty('top', `${CPCHeight}px`);
+	
+				wrapper.style.setProperty('padding-top' ,`${headerHeight}px`);
+			} else {
+				header.classList.remove("sticky");
+				
+				header.style.removeProperty('top');
+	
+				wrapper.style.removeProperty('padding-top');
+			}
+		};
+	}
 });
